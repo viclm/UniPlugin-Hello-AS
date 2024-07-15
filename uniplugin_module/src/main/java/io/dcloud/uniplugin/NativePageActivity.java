87f0f0298322d90c083e63c15d70cf594f3de4a0
Liu Ming <xliuming@outlook.com>
2024-07-15 17:05:52 +0800

    update pics

--- uniplugin_module/src/main/java/io/dcloud/uniplugin/NativePageActivity.java
+++ uniplugin_module/src/main/java/io/dcloud/uniplugin/NativePageActivity.java
@@ -14,7 +14,7 @@ import android.widget.Toast;
  * 在原生 Android 环境中使用 PaddleOCRModule 进行 OCR 功能的演示,展示如何初始化和识别文本。
  */
 public class NativePageActivity extends Activity {
-    private static final String IMAGE_PATH = "pics/alphanumeric.png";
+    private static final String IMAGE_PATH = "pics/3.jpg";
     private PaddleOCRModule ocrModule;
     private Context context;
 
